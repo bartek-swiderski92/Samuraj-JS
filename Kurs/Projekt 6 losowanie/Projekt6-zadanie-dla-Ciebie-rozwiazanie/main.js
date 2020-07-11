bde7a99be6cd0e7302c8ff4fe59632b5def08bef
Bartek <phos23@gmai.com>
2020-07-11 15:26:10 +0100

    Update

--- Kurs/Projekt 6 losowanie/Projekt6-zadanie-dla-Ciebie-rozwiazanie/main.js
+++ Kurs/Projekt 6 losowanie/Projekt6-zadanie-dla-Ciebie-rozwiazanie/main.js
@@ -1,33 +1,24 @@
 const options = ['opcja 1', 'opcja 2'];
 
-const showAdvice = () => {
- const index = Math.floor(Math.random() * options.length)
- console.log(index);
- document.querySelector('h1').textContent = options[index];
+const addOption = (event) => {
+    event.preventDefault();
+    const input = document.querySelector('input')
+    options.push(input.value);
+    console.log(options);
+    alert(`Dodana zostsała opcja ${input.value}`);
+    input.value = '';
+
+
 }
 
-const addOption = (e) => {
- e.preventDefault();
- const input = document.querySelector('input');
- options.push(input.value)
- alert(`dodana została opcja ${input.value}`);
- input.value = "";
+const resetOptions = (event) => {
+    event.preventDefault();
+    options.length = 0;
 }
-
-const resetOptions = (e) => {
- e.preventDefault();
- options.length = 0;
- document.querySelector('h1').textContent = ""
-}
-
 const showOptions = () => {
- alert(options.join(" --- --- "))
+    alert(options.join(' --- --- '));
 }
 
-document.querySelector('.add').addEventListener('click', addOption)
-
-document.querySelector('.clean').addEventListener('click', resetOptions)
-
-document.querySelector('.showAdvice').addEventListener('click', showAdvice)
-
-document.querySelector('.showOptions').addEventListener('click', showOptions)
+document.querySelector('.add').addEventListener('click', addOption);
+document.querySelector('.clean').addEventListener('click', resetOptions);
+document.querySelector('.showOptions').addEventListener('click', showOptions);
---
+++ Kurs/Projekt 6 losowanie/Projekt6-zadanie-dla-Ciebie-start/main.js
@@ -0,0 +1,66 @@
+let advices = ['Walcz!', 'Spróbuj kiedy indziej', 'Daj sobie spokój'];
+
+const btnAdd = document.querySelector('.add');
+const btnCln = document.querySelector('.clean');
+const btnAdv = document.querySelector('.showAdvice');
+const btnOpts = document.querySelector('.showOptions');
+
+
+const addAdvice = e => {
+    e.preventDefault();
+    const input = document.querySelector('input');
+    const newAdvice = input.value;
+    if (newAdvice.length) {
+        for (const advice of advices) {
+            if (advice === newAdvice) {
+                alert(`Błąd! Możliwość "${newAdvice}" już istnieje!`);
+                return;
+            }
+        }
+        alert(`Dodałem "${newAdvice}" do możlwości!`)
+        console.log('Dodałem porade!');
+        advices.push(newAdvice);
+        console.log(advices);
+        input.value = '';
+    }
+}
+
+btnAdd.addEventListener('click', addAdvice);
+
+
+const resetAdvices = e => {
+    e.preventDefault();
+    console.log('wyczyściłem tablice!')
+    advices = ['Walcz!', 'Spróbuj kiedy indziej', 'Daj sobie spokój'];
+    console.log(advices);
+    alert("Wyczyściłem możiwości!")
+}
+
+btnCln.addEventListener('click', resetAdvices);
+
+const showAdvice = () => {
+    // console.log(Math.floor(Math.random() * advices.length));
+    const index = Math.floor(Math.random() * advices.length);
+    let h1 = document.querySelector('h1');
+    console.log(advices[index]);
+    h1.textContent = advices[index];
+}
+
+btnAdv.addEventListener('click', showAdvice);
+
+const showOptions = () => {
+    alert(`Istniejące możliwośći: 
+        ${advices.join(`,
+        `)}.`)
+    // function loop() {
+    //     for (let i = 0; i < advices.length; i++) {
+    //         console.log(advices[i]);
+    //         // advices[i];
+    //     };
+    // };
+    // loop()
+    // // alert(advices[0] + ', ' + advices[1] + ', ' + advices[2]);
+    // alert(loop());
+}
+
+btnOpts.addEventListener('click', showOptions);
