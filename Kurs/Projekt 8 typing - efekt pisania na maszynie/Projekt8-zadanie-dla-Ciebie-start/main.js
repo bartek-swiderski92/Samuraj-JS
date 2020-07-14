c47f482ee14c48bf615bc85987668737272ad1fa
Phos23 <phos23@gmail.com>
2020-07-14 22:25:01 +0100

    Project 8 WIP

--- Kurs/Projekt 8 typing - efekt pisania na maszynie/Projekt8-zadanie-dla-Ciebie-start/main.js
+++ Kurs/Projekt 8 typing - efekt pisania na maszynie/Projekt8-zadanie-dla-Ciebie-start/main.js
@@ -7,10 +7,32 @@
 const spnText = document.querySelector('.text');
 const spnCursor = document.querySelector('.cursor');
 const txt = ['tekst1', 'tekst2', 'tekst3']
+let indexLetter = 0;
+let indexWord = 0;
+
+function showWord() {
+    // spnText.textContent += txt[0][0++]
+    set
+}
 
 // Implementacja
 const addLetter = () => {
- // Użyj w środku setTimeout
+    // Użyj w środku setTimeout
+    setTimeout(() => {
+        spnText.textContent += txt[indexWord][indexLetter]
+        indexLetter++;
+        if (indexLetter - 1 == txt[indexWord].length) {
+            console.log('maks letter')
+            indexLetter = 0;
+            spnText.textContent = '';
+            indexWord++;
+            if (indexWord === txt.length) {
+                console.log('maks word')
+                spnText.textContent = txt[indexWord]
+            }
+        }
+        addLetter();
+    }, 200);
 }
 
 
@@ -19,6 +41,6 @@ addLetter(); //pierwsze wywołanie
 
 // Animacja kursora (zostaw bez zmian)
 const cursorAnimation = () => {
- spnCursor.classList.toggle('active');
+    spnCursor.classList.toggle('active');
 }
 setInterval(cursorAnimation, 400);
