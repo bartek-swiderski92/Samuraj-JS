b4ca477d70cfe4a02057b22e985d062f7133fa2f
Swiderski <phos23@gmail.com>
2020-07-21 17:10:44 +0100

    Rock

--- Kurs/Projekt 7 gra papier kamień nożyczki/projekt-new/game.js
+++ Kurs/Projekt 7 gra papier kamień nożyczki/projekt-new/game.js
@@ -11,3 +11,29 @@ const game = {
     playerHand: '',
     aiHand: ''
 }
+
+const btnStart = document.querySelector('.start')
+const hands = [...document.querySelectorAll('.select img')];
+
+function handSelector() {
+    game.playerHand = this.dataset.option;
+    console.log(game.playerHand);
+    hands.forEach(hand => hand.style.boxShadow = '');
+    this.style.boxShadow = '0 0 0 4px red';
+}
+
+function aiChoice() {
+    return hands[Math.floor(Math.random() * 3)].dataset.option;
+}
+
+function startGame() {
+    if (!game.playerHand) {
+        return alert('wybierz dlon!');
+    }
+    aiChoice();
+}
+
+hands.forEach(hand => {
+    hand.addEventListener('click', handSelector)
+});
+document.querySelector('.start').addEventListener('click', startGame)
