6df49fdb2badb100600a70db5eae92d780943c16
Phos23 <phos23@gmail.com>
2020-07-23 11:56:52 +0100

    AllTasks

--- Kurs/Projekt 12 - to do app/Zadanie5/main.js
+++ Kurs/Projekt 12 - to do app/Zadanie5/main.js
@@ -1,24 +1,22 @@
 document.body.appendChild(document.createElement('form'));
 const form = document.querySelector('form');
-
 const inputAdd = form.appendChild(document.createElement('input'));
 inputAdd.classList = 'inputAdd';
 const buttonAdd = form.appendChild(document.createElement('button'));
 buttonAdd.classList = 'buttonAdd';
-buttonAdd.textContent = 'Dodaj zadanie'
-
+buttonAdd.textContent = 'Dodaj zadanie';
 document.body.appendChild(document.createElement('h2'));
 const h2 = document.querySelector('h2');
 h2.textContent = 'Liczba zadań: '
 h2.appendChild(document.createElement('span'));
 const span = document.querySelector('span');
 span.textContent = '0';
-
 const inputSearch = document.body.appendChild(document.createElement('input'));
 inputSearch.classList = 'inputSearch';
 document.body.appendChild(document.createElement('ul'));
 const ul = document.querySelector('ul');
 const tasks = [];
+const liElements = document.getElementsByTagName('li');
 
 function removeTask(e) {
     e.target.parentNode.remove()
@@ -26,7 +24,6 @@ function removeTask(e) {
     tasks.splice(index, 1);
     renderList();
     span.textContent = tasks.length;
-
 }
 
 function addTask(e) {
@@ -34,13 +31,9 @@ function addTask(e) {
     if (inputAdd.value === '') return
     console.log('click');
     let task = document.createElement('li');
-    // task.innerHTML = inputAdd.value + " <button>Usuń</button>";
     let textSpan = task.appendChild(document.createElement('span'));
     textSpan.textContent = inputAdd.value;
-    // task.textContent = inputAdd.value;
     textSpan.classList = 'text';
-
-    // btnDeleteElement = 
     task.appendChild(document.createElement('button'));
     const deleteBtn = task.querySelector('button')
     deleteBtn.textContent = 'usuń'
@@ -56,24 +49,17 @@ const renderList = () => {
         toDoElement.dataset.key = key;
         ul.appendChild(toDoElement);
         span.textContent = tasks.length;
-
     })
 }
 
-const search = function (e) {
-    const liList = [...document.querySelectorAll('li')]
-    console.log(e.target.value);
-    liList.forEach(element => {
-        const text = element.querySelectorAll('.text')
-        if (e.target.value == element.textContent) {
-            element.style.display = ''
-        } else {
-            element.style.display = 'none'
-        }
-
-    });
-
-};
+const search = (e) => {
+    const searchText = e.target.value.toLowerCase();
+    let tasksList = tasks
+    tasksList = tasksList.filter(li => li.textContent.toLowerCase().includes(searchText));
+    console.log(tasksList);
+    ul.textContent = '';
+    tasksList.forEach(li => ul.appendChild(li));
+}
 
 buttonAdd.addEventListener('click', addTask);
 inputSearch.addEventListener('input', search)
